a768eef0122fcb2d83742ab307e9040b464b8c82
Студент <student@ad.anosov.ru>
2023-05-29 09:15:02 +0500

    первое изменение

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PR_TransportCompany.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PR_TransportCompany.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
