4306fefcc9500332754a5d6b652846ddd8156821
Студент <student@ad.anosov.ru>
2023-05-30 10:00:39 +0500

    superpuper izmenenie

---
+++ PR_TransportCompany/Views/RouteWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PR_TransportCompany.Views
+{
+    public partial class RouteWindow : Window
+    {
+        public RouteWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
